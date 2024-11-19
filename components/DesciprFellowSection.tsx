f98362fec64fdc746b0a227acbaca95a7448b1dc
nitin-suryapally <nitin28suryapally@gmail.com>
2024-11-19 19:22:16 +0530

    (fix): grammer

--- components/DesciprFellowSection.tsx
+++ components/DesciprFellowSection.tsx
@@ -28,7 +28,7 @@ const DesciprFellowSection: React.FC = () => {
     >
       <div className="flex flex-col space-y-1 items-center justify-center">
         <h2 className="heading text-black-primary">
-          Become a <span className="text-blue-teal">Quantum Academy</span>{" "}
+          Become a part of <span className="text-blue-teal">Quantum Academy</span>{" "}
           and
         </h2>
         <h2 className="heading text-black-primary">Experience the difference</h2>
